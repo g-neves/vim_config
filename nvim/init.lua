0999b4f911253e8eaa2d336089266758acd8233f
Gabriel das Neves <gneves@Gabriels-MacBook-Air.local>
2023-11-27 14:07:47 -0300

    First commit

---
+++ nvim/init.lua
@@ -0,0 +1,11 @@
+require("gneves.plugins-setup")
+require("gneves.core.options")
+require("gneves.core.keymaps")
+require("gneves.core.colorscheme")
+require("gneves.plugins.comment")
+require("gneves.plugins.nvim-tree")
+require("gneves.plugins.lualine")
+require("gneves.plugins.telescope")
+require("gneves.plugins.nvim-cmp")
+
+
